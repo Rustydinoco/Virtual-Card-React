a68b03955d877aefe82d7785d4ef4e3d12430c65
Bilal Achmad Ramadhan <bilaldoank04@gmail.com>
2024-01-18 10:59:28 +0700

    Virtual Card React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+function Card() {
+  return (
+    <div className="card">
+      <Imgprofile />
+      <Content />
+    </div>
+  );
+}
+
+function Imgprofile() {
+  return (
+    <div className="img-bx">
+      <img src="manja-vitolic-gKXKBY-C-Dk-unsplash.jpg" alt="" />
+    </div>
+  );
+}
+
+function Content() {
+  return (
+    <div className="content">
+      <Detail />
+    </div>
+  );
+}
+
+function Detail() {
+  return (
+    <div className="detail">
+      <h2>
+        Bilal Achmad
+        <br />
+        <span>Fullstack Developer</span>
+      </h2>
+      <Src />
+    </div>
+  );
+}
+
+function Src() {
+  return (
+    <ul className="sci">
+      <li>
+        <button>
+          <i class="fab fa-facebook-f"></i>
+        </button>
+      </li>
+      <li>
+        {" "}
+        <button>
+          <i class="fab fa-twitter"></i>
+        </button>
+      </li>
+      <li>
+        <button>
+          <i class="fab fa-instagram"></i>
+        </button>
+      </li>
+    </ul>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
